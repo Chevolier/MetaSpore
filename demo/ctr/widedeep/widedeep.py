4ef0d68c7352f2252cca5504b739719914b0cfce
zhangchg <zhangchg@amazon.com>
2024-07-08 05:50:33 +0000

    slight update

--- demo/ctr/widedeep/widedeep.py
+++ demo/ctr/widedeep/widedeep.py
@@ -38,6 +38,8 @@ def init_spark():
     spark_confs={
         "spark.network.timeout":"500",
         "spark.submit.pyFiles":"python.zip",
+        "spark.local.dir":"/home/ec2-user/SageMaker/tmp",
+        "spark.executor.cores": 2
     }
     spark_session = ms.spark.get_session(local=local,
                                         app_name=app_name,
@@ -62,7 +64,9 @@ def stop_spark(spark):
 
 def read_dataset(spark):
     train_dataset=spark.read.parquet(train_path)
+    train_dataset = train_dataset.limit(100)
     test_dataset=spark.read.parquet(test_path)
+    test_dataset = test_dataset.limit(100)
     print('Debug -- match train dataset sample:')
     train_dataset.show(10)
     print('Debug--match train test dataset sample:')
@@ -123,7 +127,9 @@ if __name__=="__main__":
     parser.add_argument('--conf', type=str, action='store', default='', help='config file path')
     args = parser.parse_args()
     params = load_config(args.conf)
+    print('Before update:', locals())
     locals().update(params)
+    print('After update:', locals())
     spark = init_spark()
     ## read datasets
     train_dataset, test_dataset = read_dataset(spark)
