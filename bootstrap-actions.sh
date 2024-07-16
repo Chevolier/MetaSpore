d034fb4aedcb3f7f6cc1c63b2857fd69db6ddb4b
zhangchg <zhangchg@amazon.com>
2024-07-16 10:03:18 +0000

    works for master node with train2.py

---
+++ bootstrap-actions.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Installing metaspore ..."
+# Install MetaSpore
+aws s3 cp s3://spark-emr-data/wheels/metaspore-1.2.0-cp39-cp39-linux_x86_64.whl .
+sudo pip install metaspore-1.2.0-cp39-cp39-linux_x86_64.whl
+
+# Install PyTorch
+sudo pip install torch==1.11.0+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+
+# Install PySpark with S3 support
+sudo pip install https://ks3-cn-beijing.ksyuncs.com/dmetasoul-bucket/releases/spark/pyspark-3.1.2.265f9ad4ee-py2.py3-none-any.whl
+
+sudo pip install numpy==1.23.5
+
+echo "Updating java to 11 ..."
+
+wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.rpm
+sudo rpm -i amazon-corretto-11-x64-linux-jdk.rpm
+
+sudo alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-amazon-corretto/bin/java 1
+sudo alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-amazon-corretto/bin/javac 1
+
+sudo alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
+sudo alternatives --set javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac
+
+sudo mkdir -p /etc/alternatives/jre/bin
+sudo ln -s /usr/lib/jvm/java-11-amazon-corretto/bin/java /etc/alternatives/jre/bin/java
+
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+echo $JAVA_HOME
+
+# java -version
+
+echo "Installing awscli ..."
+sudo pip install awscli --ignore-installed six
+export PATH=/usr/local/bin:$PATH
+source ~/.bashrc   # or source ~/.bash_profile
+
+# sudo yum update -y
+# sudo yum install git -y
+
+echo "Configure libstdc++.so.6"
+sudo cp /mnt/notebook-env/lib/libstdc++.so.6.0.30 /lib64/
+sudo rm /lib64/libstdc++.so.6
+sudo ln -s /lib64/libstdc++.so.6.0.30 /lib64/libstdc++.so.6
+
+sudo mkdir -p /tmp/spark-events 
+
+echo "Bootstrap action completed successfully"
