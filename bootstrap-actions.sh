2e9db12fe4788433a83da43bf6d0eabd587a15d5
zhangchg <zhangchg@amazon.com>
2024-07-17 16:48:37 +0800

    add sudo in bootstrap-actions.sh

--- bootstrap-actions.sh
+++ bootstrap-actions.sh
@@ -15,19 +15,23 @@ sudo pip install numpy==1.23.5
 
 echo "Updating java to 11 ..."
 
-wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.rpm
-sudo rpm -i amazon-corretto-11-x64-linux-jdk.rpm
+# wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.rpm
+# sudo rpm -i amazon-corretto-11-x64-linux-jdk.rpm
 
-sudo alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-amazon-corretto/bin/java 1
-sudo alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-amazon-corretto/bin/javac 1
+# sudo alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-amazon-corretto/bin/java 1
+# sudo alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-11-amazon-corretto/bin/javac 1
 
-sudo alternatives --set java /usr/lib/jvm/java-11-amazon-corretto/bin/java
-sudo alternatives --set javac /usr/lib/jvm/java-11-amazon-corretto/bin/javac
+# sudo alternatives --set java /usr/lib/jvm/java-11-amazon-corretto/bin/java
+# sudo alternatives --set javac /usr/lib/jvm/java-11-amazon-corretto/bin/javac
 
-sudo mkdir -p /etc/alternatives/jre/bin
-sudo ln -s /usr/lib/jvm/java-11-amazon-corretto/bin/java /etc/alternatives/jre/bin/java
+sudo update-alternatives --set java /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-11-amazon-corretto.x86_64/bin/javac
 
-echo 'export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto' >> ~/.bashrc
+
+# sudo mkdir -p /etc/alternatives/jre/bin
+# sudo ln -s /usr/lib/jvm/java-11-amazon-corretto/bin/java /etc/alternatives/jre/bin/java
+
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64' >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
@@ -37,8 +41,8 @@ echo $JAVA_HOME
 
 echo "Installing awscli ..."
 sudo pip install awscli --ignore-installed six
-export PATH=/usr/local/bin:$PATH
-source ~/.bashrc   # or source ~/.bash_profile
+echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 
 # sudo yum update -y
 # sudo yum install git -y
@@ -46,10 +50,13 @@ source ~/.bashrc   # or source ~/.bash_profile
 echo "Configure libstdc++.so.6"
 # sudo cp /mnt/notebook-env/lib/libstdc++.so.6.0.30 /lib64/
 
-aws s3 cp s3://spark-emr-data/wheels/libstdc++.so.6.0.30 /lib64/
+sudo aws s3 cp s3://spark-emr-data/wheels/libstdc++.so.6.0.30 /lib64/
 sudo rm /lib64/libstdc++.so.6
 sudo ln -s /lib64/libstdc++.so.6.0.30 /lib64/libstdc++.so.6
 
 sudo mkdir -p /tmp/spark-events 
+
+echo 'export SPARK_HOME=/usr/lib/spark' >> ~/.bashrc
+source ~/.bashrc
 
 echo "Bootstrap action completed successfully"
